e36e5a0c1ae0d7518dafa2d2cd05d83dbd75f6aa
Jaumzinho109 <joaoh7041@gmail.com>
2023-06-06 09:23:29 -0400

    asd

---
+++ arquivotext.py
@@ -0,0 +1,4 @@
+lista = [{"nome: João, idade: 24"},
+        {"nome: Marcelo, idade: 18"},
+        {"nome: Luciano, idade: 19"},
+        {"nome: Maria, idade: 28"}]
--- mod_ex115/parte1/__init__.py
+++ mod_ex115/parte1/__init__.py
@@ -1,3 +1,11 @@
+import arquivotext
+lista = [
+        {"nome:João", "idade:24"},
+        {"nome:Marcelo", "idade:18"},
+        {"nome:Luciano", "idade:19"},
+        {"nome:Maria", "idade:28"}
+        ]
+dic = dict()
 def opcoes():
     while True:
         n = input('Sua opção: ')
@@ -8,15 +16,20 @@ def opcoes():
                 print()
                 print('Pessoas Cadastradas')
                 print('_' * 30)
+                for k, v in enumerate(lista):
+                    print(f'{v}')
                 return opcao
-                continue
             elif opcao == 2:
                 print('_' * 30)
                 print()
                 print('Novo cadastro')
                 print('_' * 30)
+                dic["nome"] = str(input('Qual o nome? '))
+                dic["idade"] = int(input('Qual a idade? '))
+                print(dic)
+                lista.append(dic.copy())
+                dic.clear()
                 return opcao
-                continue
             elif opcao == 3:
                 print('_' * 30)
                 print()
@@ -27,3 +40,4 @@ def opcoes():
                 print('Digite um dos números citados, por favor.')
         else:
             print('Não aceitamos textos, por favor digite um dos números citados.')
+
